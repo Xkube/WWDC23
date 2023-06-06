31bd335e8a5df6c150c2644f93ef72012fff7e4b
Xkube <dzikor@icloud.com>
2023-06-06 13:29:41 -0700

    Initial Commit

---
+++ WWDC23 developer/WWDC23_developerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC23_developerApp.swift
+//  WWDC23 developer
+//
+//  Created by Jimekor Hini on 2023-06-06.
+//
+
+import SwiftUI
+
+@main
+struct WWDC23_developerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
